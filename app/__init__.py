d458947a8d3fa2f839810f7ddc19cd47f7828921
Vilde Riveland <jeg.heter.vilde@gmail.com>
2020-07-31 15:12:44 +0200

    database

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,19 @@
 from flask import Flask
-from config import Config
 from flask_bootstrap import Bootstrap
+import mysql.connector
+from mysql.connector import errorcode
 
+config = {
+  'user': 'root',
+  'password': 'root',
+  'unix_socket': '/Applications/MAMP/tmp/mysql/mysql.sock',
+  'database': 'mydtb',
+  'raise_on_warnings': True,
+}
+
+conn = mysql.connector.connect(**config)
+cur = conn.cursor()
 app = Flask(__name__)
-app.config.from_object(Config)
 
 Bootstrap(app)
 
